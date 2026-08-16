36bf18c02bebd8949a86b9b7d3fcf97413bd715d
Pratham-Bajpai1 <prathamvajpai014@gmail.com>
2026-08-16 18:06:38 +0000

    fix: updated OIDC thumbprints and wildcard claim

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -1,7 +1,11 @@
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a2168627f9c908a8b0a7128d96a484382570"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a2168627f9c908a8b0a7128d96a484382570",
+    "d89e3bd43d5d909b47a1897735d741221cfa345b"
+  ]
 }
 
 resource "aws_iam_role" "github_actions_role" {
@@ -21,22 +25,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:Pratham-Bajpai1/TicketDesk:*"
-          }
-        }
-      },
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:Pratham-Bajpai1/Ticketdesk:*"
+            "token.actions.githubusercontent.com:sub" = "repo:Pratham-Bajpai1/*"
           }
         }
       }
